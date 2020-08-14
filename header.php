7e32956b92d020497affa9ede15b897a66256c5c
Benno <47930041+Benonsen@users.noreply.github.com>
2020-08-14 23:58:44 +0200

    First "real" commit
    Fixed file structure

---
+++ header.php
@@ -0,0 +1,33 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+        <img src="includes/img/logo.png"  width="40" height="40" class="d-inline-block align-top" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Projects</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Task
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">High Priority</a>
+                    <a class="dropdown-item" href="#">Medium Priority</a>
+                    <a class="dropdown-item" href="#">Lowest Priority</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Completed</a>
+                </div>
+            </li>
+        </ul>
+        </div>
+</nav>
+
