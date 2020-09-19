fc465cb106a10e0b2f25b696a03b7ad564e13de6
Benonsen <benno.steinegger02@gmail.com>
2020-09-20 01:33:59 +0200

    added prefilled fields for edit task

--- files/src.editTaskform.php
+++ files/src.editTaskform.php
@@ -3,8 +3,16 @@
     require_once '../tasklist/classes/database/class.STDMySQLDatabase.php';
     $taskedit = Taskdao::getAllTaskByUserId(); 
 //    var_dump($taskedit);
-    if(isset($_POST['task_id'])){
-            $taskeditIndex = $_POST['task_id'];
+//    if(isset($_POST['task_id'])){
+//            $taskeditIndex = $_POST['task_id'];
+//            foreach ($taskedit as $tid){
+//                echo $tid->id . ", ";
+//            }
+//    }
+    for($i = 0; $i < count($taskedit); $i++){
+        if($taskedit[$i]->id == $_POST['task_id']){
+            $indexarray = $i;
+        }
     }
     
 ?>
@@ -21,25 +29,34 @@
       <div class="modal-body">
         <form class="form-signin">
                         <div class="form-label-group">
-                            <input type="text" id="editTaskTitle" class="form-control" placeholder="Title"  value ="<?php echo $taskedit[7]->titel?>" autofocus autocomplete="off">
+                            <input type="text" id="editTaskTitle" class="form-control" placeholder="Title"  value ="<?php echo $taskedit[$indexarray]->titel?>" autofocus autocomplete="off">
                             <label for="editTaskTitle">Title</label>
                         </div>
 
                         <div class="form-label-group">
-                            <input type="text" id="editTaskDescripton" class="form-control" placeholder="Description">
+                            <input type="text" id="editTaskDescripton" class="form-control" value =" <?php echo $taskedit[$indexarray]->beschreibung ?>" placeholder="Description">
                             <label for="editTaskDescripton">Description</label>
                         </div>
             
                         <div class="form-label-group">
-                            <input type="date" id="editTaskDate" class="form-control" placeholder="Date" min="<?php echo date('Y-m-d', time())?>">
+                            <input type="date" id="editTaskDate" class="form-control" value="<?php echo date('Y-m-d', $taskedit[$indexarray]->end_date) ?>" placeholder="Date" min="<?php echo date('Y-m-d', time())?>">
+                            
                             <label for="editTaskDate">Date</label>
                         </div>
 
                         <div class="form-group">
                           <label for="EditformControlRange" style="text-align: center;">Priority</label>
-                          <input type="range" min="0" max="100" value="50" step="10" class="form-control-range" id="EditformControlRange">
+                          <input type="range" min="0" max="100" value="<?php echo $taskedit[$indexarray]->priority ?>" step="10" class="form-control-range" id="EditformControlRange">
                           <p>Value: <span id="outputPercentagePriorityEdit"></span></p>
                         </div>
+            
+                        
+                            <div class="custom-control custom-switch">
+                              <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                              <label class="custom-control-label" for="customSwitch1">Toggle this switch element</label>
+                            </div>
+
+                     
 
             <button class="btn btn-lg btn-primary btn-block text-uppercase" type="button" style="margin-top:5%;" data-dismiss="modal" onclick="editTask()">Save changes</button>
             <button class="btn btn-lg btn-primary btn-block text-uppercase" type="button" style="margin-top:3%;" data-dismiss="modal" onclick="clearFormCreateTask()">Cancel</button>
